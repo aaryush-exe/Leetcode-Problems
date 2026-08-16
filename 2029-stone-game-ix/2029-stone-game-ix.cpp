16b16635cf895e25edf18f263166e7c387fa8ca7
Aaryush <aaryushraj2007@gmail.com>
2026-08-16 19:54:49 +0530

    Time: 8 ms (30.2%), Space: 131.3 MB (69.29%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -6,13 +6,9 @@ public:
         for (int x : stones) {
             cnt[x % 3]++;
         }
-
-        // Even number of remainder-0 stones
         if (cnt[0] % 2 == 0) {
             return cnt[1] > 0 && cnt[2] > 0;
         }
-
-        // Odd number of remainder-0 stones
         return abs(cnt[1] - cnt[2]) > 2;
     }
 };
